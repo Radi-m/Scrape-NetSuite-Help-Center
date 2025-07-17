99cc817107d1ded4f4698b4e3685ed7cc6bcf5f7
Radoslaw Markun <markun.radoslaw@gmail.com>
2025-07-17 10:32:55 +0100

    secret template added

---
+++ secret_template.py
@@ -0,0 +1,11 @@
+NETSUITE_BASE_URL = 'https://<1234567>.app.netsuite.com'
+NETSUITE_LOGIN_PAGE = f'https://system.netsuite.com/pages/customerlogin.jsp?country=US'
+HELP_CENTER_URL = f'https://<1234567>.app.netsuite.com/app/help/helpcenter.nl?fid=set_1502135122.html'
+EMAIL = 'x@y.com'
+PASSWORD = '*****'
+# The 'key' must be the EXACT text of the question from the page.
+# The 'value' is the answer. Add all your possible questions.
+SECURITY_QUESTIONS = {
+    "What was your childhood nickname?": "alladin",
+    "In what city or town was your first job?": "LA",
+}
